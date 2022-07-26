d0d41d1915103cdb0a00541e619c82b66fa1e7a1
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-26 16:14:59 +0100

    REST API for goals

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -2,23 +2,103 @@ const { StatusCodes } = require('http-status-codes');
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/Goal');
+const APIFeatures = require('../utils/apiFeatures');
+const NotFoundError = require('../errors/notFound');
+const ForbiddenError = require('../errors/forbidden');
 
 exports.getAllGoals = asyncHandler(async (req, res, next) => {
-  const goals = await Goal.find();
+  const features = new APIFeatures(Goal.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const goals = await features.query;
 
   res.status(StatusCodes.OK).json({
     status: 'success',
     nbHits: goals.length,
+    requestedAt: req.requestTime,
     goals,
   });
 });
 
-exports.getGoalById = asyncHandler(async (req, res, next) => {});
+exports.getGoalById = asyncHandler(async (req, res, next) => {
+  const { id: goalId } = req.params;
 
-exports.getAllGoalBySlug = asyncHandler(async (req, res, next) => {});
+  const goal = await Goal.findById(goalId);
 
-exports.createGoal = asyncHandler(async (req, res, next) => {});
+  if (!goal) {
+    return next(new NotFoundError(`No goal found with that ID → ${goalId}`));
+  }
 
-exports.updateGoal = asyncHandler(async (req, res, next) => {});
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    goal,
+  });
+});
 
-exports.deleteGoal = asyncHandler(async (req, res, next) => {});
+exports.getAllGoalBySlug = asyncHandler(async (req, res, next) => {
+  const { slug } = req.params;
+
+  const goal = await Goal.findOne({ slug });
+
+  if (!goal) {
+    return next(new NotFoundError(`No goal found with that ID → ${slug}`));
+  }
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    goal,
+  });
+});
+
+exports.createGoal = asyncHandler(async (req, res, next) => {
+  const goal = await Goal.create({ ...req.body });
+
+  res.status(StatusCodes.CREATED).json({
+    status: 'success',
+    goal,
+  });
+});
+
+exports.updateGoal = asyncHandler(async (req, res, next) => {
+  const { id: goalId } = req.params;
+
+  const goal = await Goal.findById(goalId);
+
+  if (!goal) {
+    return next(new NotFoundError(`No goal found with that ID → ${goalId}`));
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(
+    goalId,
+    { $set: { ...req.body } },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    goal: updatedGoal,
+  });
+});
+
+exports.deleteGoal = asyncHandler(async (req, res, next) => {
+  const { id: goalId } = req.params;
+
+  const goal = await Goal.findById(goalId);
+
+  if (!goal) {
+    return next(new NotFoundError(`No goal found with that ID → ${goalId}`));
+  }
+
+  await goal.remove();
+
+  res.status(StatusCodes.NO_CONTENT).json({
+    status: 'success',
+    goal: null,
+  });
+});
