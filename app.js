d1b7aabd42efa78ed95bd332e02d3ddda7639401
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-26 16:36:56 +0100

    added middlewares

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const compression = require('compression');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 // requiring routes
 const goalRouter = require('./routes/goals');
 const globalErrorHandler = require('./middleware/errorHandler');
 const NotFoundError = require('./errors/notFound');
 
+// start express app
 const app = express();
 
+// global middlewares
+// implement CORS
+app.use(cors());
+
+// Access-Control-Allow-Origin
+app.options('*', cors());
+
+// set security HTTP headers
+app.use(helmet());
+
+// development logging
 if (app.get('env') === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit request from same api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+// body Parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+// cookie parser middleware
+app.use(cookieParser());
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss());
+
+// prevent parameter pollution
+app.use(hpp());
+
+// compression middleware
+app.use(compression());
 
 // test middleware
 app.use((req, res, next) => {
@@ -21,6 +66,7 @@ app.use((req, res, next) => {
   next();
 });
 
+// routes middleware
 app.use('/api/v1/goals', goalRouter);
 
 app.all('*', (req, res, next) =>
