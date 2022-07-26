c6a975903961728dc7d5ec3694f6936fa7feb14e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-26 16:15:30 +0100

    first commit

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,97 @@
+const { StatusCodes } = require('http-status-codes');
+const asyncHandler = require('express-async-handler');
+
+const APIFeatures = require('../utils/apiFeatures');
+const NotFoundError = require('../errors/notFound');
+
+exports.getAll = (Model) =>
+  asyncHandler(async (req, res, next) => {
+    const features = new APIFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const docs = await features.query;
+    // const docs = await features.query.explain();
+
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      nbHits: docs.length,
+      requestedAt: req.requestTime,
+      docs,
+    });
+  });
+
+exports.getOne = (Model) =>
+  asyncHandler(async (req, res, next) => {
+    const { id: docId } = req.params;
+
+    const doc = await Model.findById(docId);
+
+    if (!doc) {
+      return next(
+        new NotFoundError(`No document found with that ID → ${docId}`)
+      );
+    }
+
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      doc,
+    });
+  });
+
+exports.createOne = (Model) =>
+  asyncHandler(async (req, res, next) => {
+    const doc = await Model.create({ ...req.body });
+
+    res.status(StatusCodes.CREATED).json({
+      status: 'success',
+      doc,
+    });
+  });
+
+exports.updateOne = (Model) =>
+  asyncHandler(async (req, res, next) => {
+    const { id: docId } = req.params;
+
+    const doc = await Model.findById(docId);
+
+    if (!doc) {
+      return next(
+        new NotFoundError(`No document found with that ID → ${docId}`)
+      );
+    }
+
+    const updateddoc = await Model.findByIdAndUpdate(
+      docId,
+      { $set: { ...req.body } },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(StatusCodes.OK).json({
+      status: 'success',
+      doc: updateddoc,
+    });
+  });
+
+exports.deleteOne = (Model) =>
+  asyncHandler(async (req, res, next) => {
+    const { id: docId } = req.params;
+
+    const doc = await Model.findById(docId);
+
+    if (!doc) {
+      return next(new NotFoundError(`No doc found with that ID → ${docId}`));
+    }
+
+    await doc.remove();
+
+    res.status(StatusCodes.NO_CONTENT).json({
+      status: 'success',
+      doc: null,
+    });
+  });
