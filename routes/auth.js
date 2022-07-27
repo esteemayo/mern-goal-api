3cb23c264d76a12542f9655eabb9d432bfe0f78e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-27 02:11:23 +0100

    added other routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,16 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const protect = require('../middleware/authMiddleware');
+const authController = require('../controllers/authController');
 
-module.exports = router
+const router = express.Router();
+
+router.post('/login', authController.loginUser);
+
+router.post('/forgot-password', authController.forgotPassword);
+
+router.post('/reset-password/:token', authController.resetPassword);
+
+router.patch('/update-my-password', protect, authController.updateMyPassword);
+
+module.exports = router;
