515bc37fce2f5cfa3d2c2bcc2796dc160652740e
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-07-27 18:57:25 +0100

    added other users route endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,38 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const protect = require('../middleware/authMiddleware');
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
-module.exports = router
+const router = express.Router();
+
+router.post('/register', userController.registerUser);
+
+router.use(protect);
+
+router.get(
+  '/stats',
+  authController.restrictTo('admin'),
+  userController.getUserStats
+);
+
+router.patch('/update-me', userController.updateMe);
+
+router.get('/me', userController.getMe, userController.getUser);
+
+router.delete('/delete-me', userController.deleteMe);
+
+router
+  .route('/')
+  .get(authController.restrictTo('admin'), userController.getAllUsers)
+  .post(userController.createUser);
+
+router.use(authController.restrictTo('admin'));
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
