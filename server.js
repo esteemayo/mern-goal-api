a22752201b239417700ffa02405656d4167dc1aa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-06 00:57:00 +0100

    fix: refactor code

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ process.on('uncaughtException', (err) => {
 
 dotenv.config({ path: './config.env' });
 
-connectDB();
-
 app.set('port', process.env.PORT || 7777);
 
 const server = app.listen(app.get('port'), async () => {
