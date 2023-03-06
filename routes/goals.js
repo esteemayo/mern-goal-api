552a71a74baa5e72a0f65b62232b78a2316e06a2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-07 00:29:36 +0100

    fix: modify

--- routes/goals.js
+++ routes/goals.js
@@ -1,6 +1,5 @@
 import express from 'express';
 
-import authController from '../controllers/authController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 import goalController from '../controllers/goalController.js';
 
