961e99188d86df0bd4dbd244356b06aa0461735e
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-06 01:14:42 +0100

    fix: modify

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import protect from '../middleware/authMiddleware.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 import authController from '../controllers/authController.js';
 
 const router = express.Router();
